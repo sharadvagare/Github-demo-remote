ce43933e8173fbb1b575249a21ce871fb30c464d
sharad_vagare <sharad_vagare@persistent.com>
2022-07-17 11:20:52 +0530

    added fn headers

--- math.py
+++ math.py
@@ -1,11 +1,15 @@
+# add impl
 def add(x,y):
     pass
 
+#subtract impl
 def subtract(x,y):
     pass
-   
+
+#multiply impl   
 def multiply(x,y):
     pass
     
+#divide impl    
 def divide(x,y):
     pass
